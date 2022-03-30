24e099e13469c62c12ad878628dc19fa01ad3c5c
yangpenghui <yangpenghui@qq.com>
2022-03-30 18:16:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yangpenghui
+ * @create 2022-03-30 18:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
